e8ce2ec83539405c9f859d3e6589f6c2c6f5cea8
flycal6 <brianraythomas@gmail.com>
2016-02-28 11:54:14 +0300

    user login view url and template complete

--- moneykatz/urls.py
+++ moneykatz/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns(
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_file/$', views.add_file, name='add_file'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
 )
--- moneykatz/views.py
+++ moneykatz/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseRedirect
 from moneykatz.models import Category, File
 from moneykatz.forms import CategoryForm, FileForm, UserProfileForm, UserForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/moneykatz/')
+
+            else:
+                return HttpResponse('Your account is disabled.')
+
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        return render(request, 'moneykatz/login.html', {})
 
 
 def register(request):
