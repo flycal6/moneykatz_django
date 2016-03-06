839e0516e83e5969e25bb627901f8673fbed1d04
flycal6 <brianraythomas@gmail.com>
2016-03-06 21:53:23 +0300

    prepended staticfiles_dirs with path to glyphicons

--- moneykatz_django/settings.py
+++ moneykatz_django/settings.py
@@ -97,7 +97,7 @@ STATIC_URL = 'http://drycountrybrewing.com/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    ''
+    '/home/flycal6/webapps/static/fonts'
     '/home/flycal6/webapps/mkatzdjango/moneykatz_django/media/media/',
     '/home/flycal6/webapps/mkatzdjango/moneykatz_django/media/uploads',
 )
