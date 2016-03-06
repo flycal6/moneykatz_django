d7958ff972267660614dc92bac4b08dd18906946
flycal6 <brianraythomas@gmail.com>
2016-03-06 21:09:13 +0300

    index url links fixed

--- moneykatz_django/urls.py
+++ moneykatz_django/urls.py
@@ -21,7 +21,7 @@ urlpatterns = patterns('',
                        url(r'^moneykatz/', include('moneykatz.urls')),
                        url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
                        url(r'^accounts/', include('registration.backends.simple.urls')),
-                       url(r'^$', views.index, name='index'),
+                       url(r'^', views.index, name='index'),
                        )
 if settings.DEBUG:
     urlpatterns += patterns(
