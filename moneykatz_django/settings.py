19682aa7c2a7a670c1249df966f41964e269c76b
flycal6 <brianraythomas@gmail.com>
2016-03-19 21:30:26 +0300

    renamed settings.py to settings_dev.py

--- moneykatz_django/settings.py
+++ moneykatz_django/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moneykatz.com', 'www.moneykatz.com', 'www.drycountrybrewing.com', 'drycountrybrewing.com']
 
 # Application definition
 
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'moneykatz_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'moneykatz_database',
+        'USER': 'flycal6',
+        'PASSWORD': 'Kernut1!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -87,12 +91,14 @@ LOGIN_URL = '/accounts/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
+STATIC_ROOT = '/home/flycal6/webapps/static/'
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://moneykatz.com/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    '/home/flycal6/webapps/mkatzdjango/moneykatz_django/media/media/',
+    '/home/flycal6/webapps/mkatzdjango/moneykatz_django/media/uploads',
 )
 
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
@@ -100,9 +106,9 @@ TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_DIRS = (TEMPLATE_PATH,
                  )
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://moneykatz.com/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/flycal6/webapps/media/'
 
 # Registration Redux
 
@@ -126,3 +132,6 @@ EMAIL_HOST_USER = host_user
 EMAIL_HOST_PASSWORD = host_password
 EMAIL_USE_TLS = True
 EMAIL_PORT = port
+
+ADMINS = (admin1, admin2, admin3)
+SERVER_EMAIL = admin1
